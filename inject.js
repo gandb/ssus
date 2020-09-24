85658bbe27f917836eefd0ceb4c19626924bcfc0
gandb <gandbranco@gmail.com>
2020-09-24 14:26:45 -0300

    Melhorado o load

--- inject.js
+++ inject.js
@@ -1,27 +1,31 @@
 
-document.txtAndamento = null;
-document.cmbSituacao = null;
+(()=>{
+        
+    document.txtAndamento = null;
+    document.cmbSituacao = null;
 
-const textAreas = document.getElementsByTagName("textarea");
+    const textAreas = document.getElementsByTagName("textarea");
 
-for(let index = 0 ; index < textAreas.length ; index++)
-{
-    const textArea = textAreas.item(index);
-    if(textArea.id.indexOf('txtAndamento')>=0)
+    for(let index = 0 ; index < textAreas.length ; index++)
     {
-        document.txtAndamento = textArea;
-    }    
-}
-
-const selects = document.getElementsByTagName("select");
-for(let index = 0 ; index < selects.length ; index++)
-{
-    const select = selects.item(index);
-    if(select.id.indexOf('Situacao')>=0)
-    {
-        document.cmbSituacao = select;
+        const textArea = textAreas.item(index);
+        if(textArea.id.indexOf('txtAndamento')>=0)
+        {
+            document.txtAndamento = textArea;
+        }    
     }
-}
+
+    const selects = document.getElementsByTagName("select");
+    for(let index = 0 ; index < selects.length ; index++)
+    {
+        const select = selects.item(index);
+        if(select.id.indexOf('Situacao')>=0)
+        {
+            document.cmbSituacao = select;
+        }
+    }
+})();
+
 
 function checkPage()
 { 
